1a378c1b6be6cb8f564efa249d203bd389863bd8
Keremimo <nyaa@live.com>
2024-11-19 14:03:30 +0100

    Add remote sshfs

--- init.lua
+++ init.lua
@@ -199,6 +199,7 @@ require('lazy').setup({
       -- Enable Telescope extensions if they are installed
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
+      pcall(require('telescope').load_extension, 'remote-sshfs')
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
