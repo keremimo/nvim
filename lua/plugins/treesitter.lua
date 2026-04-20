8fc7a7cccc364b5443d7cbfab1d1b9b50275ffd4
Kerem Kilic <git@keremk.be>
2026-04-20 21:51:31 +0200

    fix(nvim): pin treesitter to master API
    Switch nvim-treesitter to the legacy master branch so configs-based setup keeps working and Neovim no longer errors on startup.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'master',
     event = { 'BufReadPost', 'BufNewFile' },
     cmd = { 'TSInstall', 'TSUpdate', 'TSUninstall', 'TSBufEnable', 'TSBufDisable', 'TSModuleInfo' },
     build = ':TSUpdate',
